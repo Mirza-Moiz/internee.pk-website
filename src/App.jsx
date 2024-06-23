c7308a7e5d59229e403992c8cc35f4b429a39fa8
Mirza-Moiz <moiznadeem1243@gmail.com>
2024-06-23 15:49:20 +0500

    Added: Not Found Page

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,21 @@ import {
 
 import MainLayout from "./layout/MainLayout";
 import HomePage from "./pages/HomePage";
+import ContactPage from "./pages/ContactPage";
+import CompanyPage from "./pages/CompanyPage";
+import InternshipPage from "./pages/InternshipPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<MainLayout />}>
         <Route index element={<HomePage />} />
-        {/* <Route path="*" element={<NotFoundPage />} /> */}
+        <Route path="/internship" element={<InternshipPage />} />
+        <Route path="/company" element={<CompanyPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+
+        <Route path="*" element={<NotFoundPage />} />
       </Route>
     )
   );
---
+++ src/pages/InternshipPage.jsx
@@ -0,0 +1,5 @@
+const InternshipPage = () => {
+  return <div>InternshipPage</div>;
+};
+
+export default InternshipPage;
