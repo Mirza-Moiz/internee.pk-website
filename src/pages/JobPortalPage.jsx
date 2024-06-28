4e6752688bbd91013f30cc5802f09b50dbc99b06
Mirza-Moiz <moiznadeem1243@gmail.com>
2024-06-28 22:48:22 +0500

    Fixed: Job Portal page Text property

--- src/pages/JobPortalPage.jsx
+++ src/pages/JobPortalPage.jsx
@@ -6,7 +6,7 @@ const JobPortalPage = () => {
     <>
       <BackButton />
       <div className="flex items-center justify-center h-screen">
-        <div className="text-[#43a724] p-2 text-5xl sm:text-6xl font-semibold min-h-48 sm:min-h-36 ">
+        <div className="text-[#43a724] p-2 text-5xl text-center sm:text-6xl font-semibold min-h-48 sm:min-h-36 ">
           <ReactTyped
             startWhenVisible
             strings={["Coming Soon!!!", "Job Portal", "Stay Tuned!"]}
